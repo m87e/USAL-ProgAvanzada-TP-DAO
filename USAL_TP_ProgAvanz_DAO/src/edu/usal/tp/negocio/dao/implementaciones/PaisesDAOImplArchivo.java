b32790f3ac7f691af7d2300c6508d6bb732f4b02
juan <juan@192.168.1.19>
2019-05-31 21:44:46 -0300

    Updated PaisesDAOImplArchivo

--- USAL_TP_ProgAvanz_DAO/src/edu/usal/tp/negocio/dao/implementaciones/PaisesDAOImplArchivo.java
+++ USAL_TP_ProgAvanz_DAO/src/edu/usal/tp/negocio/dao/implementaciones/PaisesDAOImplArchivo.java
@@ -1,16 +1,37 @@
 package edu.usal.tp.negocio.dao.implementaciones;
 
-import java.io.IOException;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 import edu.usal.tp.negocio.dao.dominio.Paises;
 import edu.usal.tp.negocio.dao.interfaces.IPaisesDAO;
 
 public class PaisesDAOImplArchivo implements IPaisesDAO {
 
+	private File archivo;
+	private FileWriter archivoWriter;
+	private FileReader archivoReader;
+	private BufferedWriter archivoBufferWriter;
+	private BufferedReader archivoBufferReader;
+
 	@Override
 	public void AgregarPais(Paises pais) throws IOException {
 		// TODO Auto-generated method stub
+
+		archivo = new File("/Users/juan/Desktop/paises.txt");
+		archivoWriter = new FileWriter(archivo);
+		archivoBufferWriter = new BufferedWriter(archivoWriter);
+
+		String str = SavePais(pais);
+
+		archivoBufferWriter.write(str);
+		archivoBufferWriter.close();
+		archivoWriter.close();
+
+	}
+
+	private String SavePais(Paises pais) {
+		return pais.getId() + ";" + pais.getNombre() + "\n";
 
 	}
 
@@ -29,7 +50,31 @@ public class PaisesDAOImplArchivo implements IPaisesDAO {
 	@Override
 	public List<Paises> GetAll() throws IOException {
 		// TODO Auto-generated method stub
-		return null;
+		archivo = new File("/Users/juan/Desktop/paises.txt");
+		archivoReader = new FileReader(archivo);
+		archivoBufferReader = new BufferedReader(archivoReader);
+
+		String linea;
+		List<Paises> listadoPaises = new ArrayList<>();
+
+		while ((linea = archivoBufferReader.readLine()) != null) {
+			listadoPaises.add(ParsePais(linea));
+		}
+
+		return listadoPaises;
+
+	}
+
+	private Paises ParsePais(String linea) {
+
+		String[] atributos = linea.split(";");
+
+		Paises pais = new Paises();
+		pais.setId(atributos[0]);
+		pais.setNombre(atributos[1]);
+
+		return pais;
+
 	}
 
 }
