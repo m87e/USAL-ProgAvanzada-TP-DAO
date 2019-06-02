dac748af5d970b566f1a8b097f77645772021394
juan <juan@192.168.1.19>
2019-06-02 16:16:22 -0300

    Modified Aerolinea and Added AerolineaDAOimplarchivo

---
+++ USAL_TP_ProgAvanz_DAO/src/edu/usal/tp/negocio/dao/implementaciones/AerolineaDAOImplArchivo.java
@@ -0,0 +1,81 @@
+package edu.usal.tp.negocio.dao.implementaciones;
+
+import java.io.*;
+import java.util.*;
+
+import edu.usal.tp.negocio.dao.dominio.Aerolinea;
+import edu.usal.tp.negocio.dao.interfaces.IAerolineaDAO;
+
+public class AerolineaDAOImplArchivo implements IAerolineaDAO {
+
+	private File archivo;
+	private FileWriter archivoWriter;
+	private FileReader archivoReader;
+	private BufferedWriter archivoBufferWriter;
+	private BufferedReader archivoBufferReader;
+
+	@Override
+	public void AgregarAerolinea(Aerolinea aerolinea) throws IOException {
+		// TODO Auto-generated method stub
+		archivo = new File("/Users/juan/Desktop/aerolineas.txt");
+		archivoWriter = new FileWriter(archivo);
+		archivoBufferWriter = new BufferedWriter(archivoWriter);
+
+		String str = SaveAerolinea(aerolinea);
+
+		archivoBufferWriter.write(str);
+		archivoBufferWriter.close();
+		archivoWriter.close();
+
+	}
+
+	private String SaveAerolinea(Aerolinea aerolinea) {
+		// TODO Auto-generated method stub
+		return aerolinea.getId() + ";" + aerolinea.getNombre() + ";" + aerolinea.getAlianza() + "\n";
+	}
+
+	@Override
+	public void ModificarAerolinea(Aerolinea oldAerolinea, Aerolinea newAerolinea) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void EliminarAerolinea(Aerolinea pais) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Aerolinea> GetAll() throws IOException {
+		// TODO Auto-generated method stub
+
+		archivo = new File("/Users/juan/Desktop/aerolineas.txt");
+		archivoReader = new FileReader(archivo);
+		archivoBufferReader = new BufferedReader(archivoReader);
+
+		String linea;
+
+		List<Aerolinea> listadoAerolineas = new ArrayList<>();
+
+		while ((linea = archivoBufferReader.readLine()) != null) {
+			listadoAerolineas.add(parseAerolinea(linea));
+		}
+
+		return null;
+
+	}
+
+	private Aerolinea parseAerolinea(String linea) {
+
+		String[] atributos = linea.split(";");
+
+		Aerolinea aerolinea = new Aerolinea();
+		aerolinea.setId(atributos[0]);
+		aerolinea.setNombre(atributos[1]);
+		aerolinea.setAlianza(atributos[2]);
+
+		return aerolinea;
+	}
+
+}
