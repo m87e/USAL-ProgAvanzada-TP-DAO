c1870fe33180449dff5e7074f581a83ac95d1d15
juan <juan@192.168.1.21>
2019-06-05 20:06:01 -0300

    updated attribute Ciudad -> ciudad

--- USAL_TP_ProgAvanz_DAO/src/edu/usal/tp/negocio/dao/dominio/Aeropuerto.java
+++ USAL_TP_ProgAvanz_DAO/src/edu/usal/tp/negocio/dao/dominio/Aeropuerto.java
@@ -2,46 +2,53 @@ package edu.usal.tp.negocio.dao.dominio;
 
 public class Aeropuerto {
 	private String idAeropuerto;
-	private String Ciudad;
+	private String ciudad;
 	private Provincias provincia;
 	private Paises pais;
+
 	public String getIdAeropuerto() {
 		return idAeropuerto;
 	}
+
 	public void setIdAeropuerto(String idAeropuerto) {
 		this.idAeropuerto = idAeropuerto;
 	}
+
 	public String getCiudad() {
-		return Ciudad;
+		return ciudad;
 	}
+
 	public void setCiudad(String ciudad) {
-		Ciudad = ciudad;
+		this.ciudad = ciudad;
 	}
+
 	public Provincias getProvincia() {
 		return provincia;
 	}
+
 	public void setProvincia(Provincias provincia) {
 		this.provincia = provincia;
 	}
+
 	public Paises getPais() {
 		return pais;
 	}
+
 	public void setPais(Paises pais) {
 		this.pais = pais;
 	}
+
 	public Aeropuerto(String idAeropuerto, String ciudad, Provincias provincia, Paises pais) {
-		
+
 		this.idAeropuerto = idAeropuerto;
-		Ciudad = ciudad;
+		this.ciudad = ciudad;
 		this.provincia = provincia;
 		this.pais = pais;
 	}
+
 	public Aeropuerto() {
-		
+
 		// TODO Auto-generated constructor stub
 	}
-	
-	
-	
-	
+
 }
